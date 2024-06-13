33fdacf2367daa48c263c37cd71c1d1255ec70f5
Nikolai <nvenchuk@mail.ru>
2024-06-13 18:43:33 +0700

    домашняя работа к третьему  семинару

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'seminar1',
+    'seminar1', 'myproject',
 ]
 
 MIDDLEWARE = [
